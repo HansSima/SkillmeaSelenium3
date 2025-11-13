e66118741e345dafc381db1bf1f5658edf7f98a3
Jan Sima <simahonza@seznam.cz>
2025-11-13 20:54:23 +0100

    Pridani knihoven

--- src/main/java/org/example/Login.java
+++ src/main/java/org/example/Login.java
@@ -1,31 +1,27 @@
 package org.example;
 
-import java.util.regex.Pattern;
-import java.util.concurrent.TimeUnit;
 import org.junit.*;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
-import org.apache.commons.io.FileUtils;
-import java.io.File;
 
 public class Login {
-  private WebDriver driver;
+    private WebDriver driver;
 
-  @Before
-  public void setUp() {
-    System.setProperty("webdriver.chrome.driver", "");
-    driver = new ChromeDriver();
-  }
+    @Before
+    public void setUp() {
+        //Prepsat cestu u sveho projektu na webdriver
+        System.setProperty("webdriver.chrome.driver", "D:\\StudijniMaterialy\\Studijni_material\\Studijni_material\\11_Selenium\\Kod\\SkillmeaSelenium3\\src\\main\\resources\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
 
-  @Test
-  public void testLogin() {
-    driver.get("https://www.tutorialspoint.com/selenium/practice/login.php");
-    driver.findElement(By.id("password")).sendKeys("Heslo12345");
-    driver.findElement(By.id("email")).sendKeys("novyemail@novyemail.com");
-    driver.findElement(By.xpath("//input[@value='Login']")).click();
-    assertEquals("Welcome, Login In", driver.findElement(By.xpath("//*[@id=\"signInForm\"]/h1")).getText());
-  }
+    @Test
+    public void testLogin() {
+        driver.get("https://www.tutorialspoint.com/selenium/practice/login.php");
+        driver.findElement(By.id("password")).sendKeys("Heslo12345");
+        driver.findElement(By.id("email")).sendKeys("novyemail@novyemail.com");
+        driver.findElement(By.xpath("//input[@value='Login']")).click();
+        assertEquals("Welcome, Login In", driver.findElement(By.xpath("//*[@id=\"signInForm\"]/h1")).getText());
+    }
 }
